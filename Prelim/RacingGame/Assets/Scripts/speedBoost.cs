c5e830506849a96416e4d9bec0b84aa4eedbb8fa
Denise Suarez <suarez.dma@gmail.com>
2020-11-10 13:24:17 +0800

    Movement Script
    I think I deleted the movement script by accident when I removed the AI scripts :(

---
+++ Prelim/RacingGame/Assets/Scripts/speedBoost.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class speedBoost : MonoBehaviour
+{
+    public float boostCooldown = 5f;
+     public float boostDuration = 2f;
+     private float speedIncrease = 3;
+ 
+     private bool hasCooldown;
+     private Vector3 normalMovementVector = Vector3.forward;
+     private Vector3 currentMovementVector;
+ 
+     void Start()
+     {
+         currentMovementVector = normalMovementVector;
+         StartCoroutine(ActivateCooldown());
+     }
+     
+     void Update()
+     {
+         if(Input.GetKeyDown(KeyCode.Space) && !hasCooldown)
+         {
+             currentMovementVector += Vector3.forward * speedIncrease;
+             StartCoroutine(ActivateCooldown());
+             StartCoroutine(ResetMovementVector());
+         }
+         transform.Translate(currentMovementVector * Time.deltaTime);
+     }
+ 
+     IEnumerator ResetMovementVector()
+     {
+         yield return new WaitForSeconds(boostDuration);
+         currentMovementVector = normalMovementVector;
+         Debug.Log("boost ended");
+     }
+ 
+     IEnumerator ActivateCooldown()
+     {
+         hasCooldown = true;
+         yield return new WaitForSeconds(boostCooldown);
+         hasCooldown = false;
+         Debug.Log("boost ready");
+     }
+}
